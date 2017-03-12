3dd03d0d2f639ecc177fcc991b7c288f481e8b4d
Csaba Apagyi <csaba.apagyi@gmail.com>
2017-03-12 21:36:41 +0100

    Add OCRA entry point and dependency

---
+++ ocra/poe-sniper.rb
@@ -0,0 +1,17 @@
+require_relative '../lib/poe/sniper.rb'
+require 'timeout'
+
+if File.file? 'config.ini'
+  Poe::Sniper.run('config.ini')
+else
+  # Ignore this message during OCRA build.
+  p 'Config not found. Place your config.ini file next to this executable.'
+  p 'Press a key to exit (automatically exiting in 5 seconds)'
+  # Exit timeout is needed so user can read this message but OCRA build will not hang
+  begin
+    Timeout::timeout(2) do
+      gets
+    end
+  rescue Timeout::Error
+  end
+end
