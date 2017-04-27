dd81edff212de6d7f81d9dffc55110a5ef5adc2b
Csaba Apagyi <csaba.apagyi@gmail.com>
2017-04-27 10:03:37 +0200

    Fix timeout value, add further comments

--- ocra/poe-sniper.rb
+++ ocra/poe-sniper.rb
@@ -5,11 +5,11 @@ if File.file? 'config.ini'
   Poe::Sniper.run('config.ini')
 else
   # Ignore this message during OCRA build.
-  p 'Config not found. Place your config.ini file next to this executable.'
+  p 'Config not found. Place your config.ini file next to this executable. (Ignore this error during CI or OCRA runs)'
   p 'Press a key to exit (automatically exiting in 5 seconds)'
   # Exit timeout is needed so user can read this message but OCRA build will not hang
   begin
-    Timeout::timeout(2) do
+    Timeout::timeout(5) do
       gets
     end
   rescue Timeout::Error
