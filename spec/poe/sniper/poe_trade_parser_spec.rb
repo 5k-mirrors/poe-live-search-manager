fa0c547db1dd6d213211726882373655bbbc6f43
Csaba Apagyi <csaba.apagyi@gmail.com>
2017-04-27 15:55:39 +0200

    Add single item example data and a test using it

--- spec/poe/sniper/poe_trade_parser_spec.rb
+++ spec/poe/sniper/poe_trade_parser_spec.rb
@@ -8,9 +8,30 @@ RSpec.describe PoeTradeParser do
   end
 
   describe '#get_whispers' do
-    context '' do
-      it 'should ' do
-        example_data = JsonHelper.parse_file(@socket_data_path)
+    context 'When provided with socket data in JSON format' do
+      it 'should create `Whisper` object with corresponding fields' do
+        example_data = JsonHelper.parse_file("#{RSPEC_ROOT}/resources/socket_data_single_item.json")
+        whispers = PoeTradeParser.new(example_data).get_whispers
+
+        expect(whispers.length).to eq(1)
+
+        whisper = whispers[0]
+
+        expect(whisper.ign).to eq('StarDubswithBow')
+        expect(whisper.item).to eq("Wrath Charm Gold Amulet")
+        expect(whisper.buyout).to eq("2 exalted")
+        expect(whisper.league).to eq("Breach")
+        expect(whisper.tab).to eq("30c+")
+        expect(whisper.x).to eq(0)
+        expect(whisper.y).to eq(0)
+      end
+    end
+  end
+
+  describe '#get_whispers' do
+    context 'When provided with socket data in JSON format conataining multiple items' do
+      it 'should create corresponding number of `Whisper` objects' do
+        example_data = JsonHelper.parse_file("#{RSPEC_ROOT}/resources/example_socket_data.json")
         whispers = PoeTradeParser.new(example_data).get_whispers
         expect(whispers.length).to eq(25)
       end
