4969558193051d8b0efc68e5c9fd1864e4f1bca1
Csaba Apagyi <csaba.apagyi@gmail.com>
2018-03-25 14:40:16 +0200

    Adds test for `alert_next`

---
+++ spec/poe/sniper/alerts_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+RSpec.describe Poe::Sniper::Alerts do
+  describe '#alert_next' do
+    it 'tells alert to show notification and copy itself to the clipboard' do
+      described_instance = described_class.new(1, 1)
+      alert = double('alert')
+      described_instance.push(alert)
+
+      expect(alert).to receive(:show_notification).and_return(Thread.new {})
+      expect(alert).to receive(:to_clipboard)
+
+      described_instance.alert_next
+    end
+  end
+end
