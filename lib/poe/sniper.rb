e1977a8ce4b072db73f07950a513fab923cf0fb5
Csaba Apagyi <csaba.apagyi@gmail.com>
2018-06-09 22:39:40 +0200

    Removes some of the debug output

--- lib/poe/sniper.rb
+++ lib/poe/sniper.rb
@@ -3,15 +3,11 @@ require 'dotenv'
 # See: https://github.com/larsch/ocra#running-your-application
 # Files need to be referenced relative to entry point (ocra/poe-sniper.rb)
 if defined?(Ocra)
-  p 'ocra build.....'
   Dotenv.load("#{File.dirname($0)}/../.env")
   ENV['SSL_CERT_FILE'] = "#{File.dirname($0)}/../#{ENV['SSL_CERT_FILE']}"
 else
-  p 'non-ocra run'
   Dotenv.load(".env")
 end
-
-p ENV
 
 require_relative 'sniper/poe_sniper'
 require_relative 'sniper/analytics'
@@ -19,12 +15,13 @@ require_relative 'sniper/analytics'
 module Poe
   module Sniper
     def self.run(config_path)
-      p ENV
       Analytics.ensure_analytics_key!
       Analytics.ensure_certificates!
 
       # Do not start during Ocra build 
       return if defined?(Ocra)
+
+      p ENV
 
       PoeSniper.new(config_path).run
     end
