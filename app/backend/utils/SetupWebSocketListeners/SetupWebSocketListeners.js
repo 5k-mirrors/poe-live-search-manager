d883c315f22078639ee1bb90c5683f70a0f12adf
gomorizsolt <gomori.zsolt1995@gmail.com>
2019-05-07 14:43:50 +0200

    Establish communicating from backend to frontend

--- app/backend/utils/SetupWebSocketListeners/SetupWebSocketListeners.js
+++ app/backend/utils/SetupWebSocketListeners/SetupWebSocketListeners.js
@@ -1,4 +1,5 @@
-import { Notification } from "electron";
+import { BrowserWindow, Notification } from "electron";
+import { ipcEvents } from "../../../resources/IPCEvents/IPCEvents";
 
 const doNotify = ({ notificationMessage }) => {
   new Notification({
@@ -7,11 +8,21 @@ const doNotify = ({ notificationMessage }) => {
   }).show();
 };
 
+const getCurrentWindow = () => {
+  const allWindows = BrowserWindow.getAllWindows();
+
+  return allWindows[0];
+};
+
 const setupWebSocketListeners = webSocket => {
   webSocket.on("message", message => {
     doNotify({
       notificationMessage: message
     });
+
+    const currentWindow = getCurrentWindow();
+
+    currentWindow.webContents.send(ipcEvents.ON_MESSAGE, "test");
   });
 };
 
--- app/frontend/components/Screens/Trade/Trade.js
+++ app/frontend/components/Screens/Trade/Trade.js
@@ -1,9 +1,18 @@
 import React, { useState } from "react";
 import MaterialTable from "material-table";
+import { ipcEvents } from "../../../../resources/IPCEvents/IPCEvents";
 import * as MaterialTableColumns from "../../../resources/MaterialTableColumns/MaterialTableColumns";
+
+// https://github.com/electron/electron/issues/7300#issuecomment-274269710
+const electron = window.require("electron");
+const { ipcRenderer } = electron;
 
 const trade = () => {
   const [messages, setMessages] = useState([]);
+
+  ipcRenderer.on(ipcEvents.ON_MESSAGE, (_, msg) => {
+    console.log(`[Message received.] -> ${msg}`);
+  });
 
   return (
     <MaterialTable
