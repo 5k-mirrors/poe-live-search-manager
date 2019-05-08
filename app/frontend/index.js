994fa23a1610b1712016b5319888a3ab26628e26
gomorizsolt <gomori.zsolt1995@gmail.com>
2019-05-08 19:59:13 +0200

    Move the setupIPCEvents function into index.js

--- app/frontend/index.js
+++ app/frontend/index.js
@@ -1,14 +1,26 @@
 import React, { Fragment } from "react";
+import { ipcRenderer } from "electron";
+import Store from "electron-store";
 import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { AppContainer as ReactHotAppContainer } from "react-hot-loader";
 import "../app.global.css";
 import App from "./containers/App/App";
-import setupIPCEvents from "./SetupIPCEvents/SetupIPCEvents";
+import { ipcEvents } from "../resources/IPCEvents/IPCEvents";
 
 const AppContainer = process.env.PLAIN_HMR ? Fragment : ReactHotAppContainer;
 
-setupIPCEvents();
+const store = new Store();
+
+ipcRenderer.on(ipcEvents.MESSAGE, (_, message) => {
+  const parsedMessage = JSON.parse(message);
+
+  const currentMessages = store.get("messages") || [];
+
+  currentMessages.unshift(parsedMessage);
+
+  store.set("messages", currentMessages);
+});
 
 render(
   <BrowserRouter>
