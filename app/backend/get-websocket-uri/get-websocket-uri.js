3acd2a2d3884d2eec5ea7043f2e55841b07c836c
gomorizsolt <gomori.zsolt1995@gmail.com>
2019-06-23 10:13:22 +0200

    Refine get-websocket-uri

--- app/backend/get-websocket-uri/get-websocket-uri.js
+++ app/backend/get-websocket-uri/get-websocket-uri.js
@@ -1,42 +1,10 @@
 import * as baseUrls from "../../resources/BaseUrls/BaseUrls";
-
-const isLiveUrl = splittedPathname => splittedPathname.includes("live");
-
-const lastElementIsSlash = splittedPathname =>
-  splittedPathname[splittedPathname.length - 1] === "";
-
-const getLeague = pathname => pathname.split("search/")[1].split("/")[0];
-
-const handleLiveUrl = splittedPathname => {
-  if (lastElementIsSlash(splittedPathname)) {
-    return splittedPathname[splittedPathname.length - 3];
-  }
-
-  return splittedPathname[splittedPathname.length - 2];
-};
-
-const getId = pathname => {
-  const splittedPathname = pathname.split("/");
-
-  let id = splittedPathname[splittedPathname.length - 1];
-
-  if (isLiveUrl(splittedPathname)) {
-    id = handleLiveUrl(splittedPathname);
-  }
-
-  if (!isLiveUrl(splittedPathname) && lastElementIsSlash(splittedPathname)) {
-    id = splittedPathname[splittedPathname.length - 2];
-  }
-
-  return id;
-};
+import * as regExes from "../../resources/RegExes/RegExes";
 
 const getWebSocketUri = url => {
-  const parsedUrl = new URL(url);
+  const matchDetails = url.match(regExes.searchUrlLeagueAndIdMatcher);
 
-  const league = getLeague(parsedUrl.pathname);
-
-  const id = getId(parsedUrl.pathname);
+  const [, league, id] = matchDetails;
 
   return `${baseUrls.poeWsUri}/${league}/${id}`;
 };
