817eedf87e5b604a88b17a01e3a5182704036887
gomorizsolt <gomori.zsolt1995@gmail.com>
2019-06-24 21:58:40 +0200

    Remove unnecessary javaScriptUtils.isDefined call from Connected.js

--- app/frontend/components/CustomTableColumns/Connected/Connected.js
+++ app/frontend/components/CustomTableColumns/Connected/Connected.js
@@ -1,13 +1,8 @@
 import React from "react";
 import SuccessIcon from "../../UI/Icons/Success/Success";
 import ErrorIcon from "../../UI/Icons/Error/Error";
-import * as javaScriptUtils from "../../../../utils/JavaScriptUtils/JavaScriptUtils";
 
 const connected = ({ ...props }) => {
-  if (!javaScriptUtils.isDefined(props)) {
-    return <ErrorIcon />;
-  }
-
   const { isConnected } = props;
 
   if (isConnected) {
