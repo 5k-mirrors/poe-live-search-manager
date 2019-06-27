05702fe45ff3f069ec9be41e32fed05052a6e57e
gomorizsolt <gomori.zsolt1995@gmail.com>
2019-06-27 09:12:25 +0200

    Refine toolbar UI

--- app/frontend/resources/Routes/Routes.js
+++ app/frontend/resources/Routes/Routes.js
@@ -1,5 +1,5 @@
 export const routes = [
   { displayName: "Trade", routePath: "/trade" },
-  { displayName: "Input", routePath: "/input" },
+  { displayName: "Searches", routePath: "/input" },
   { displayName: "Account", routePath: "/account" }
 ];
