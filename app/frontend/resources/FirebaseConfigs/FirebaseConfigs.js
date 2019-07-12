8dfe63f88eff9d9a33b926ad436fdd1ce8020cca
Gömöri Zsolt <gomori.zsolt1995@gmail.com>
2019-07-12 13:24:40 +0200

    Update environment variable name in FirebaseConfigs.js

--- app/frontend/resources/FirebaseConfigs/FirebaseConfigs.js
+++ app/frontend/resources/FirebaseConfigs/FirebaseConfigs.js
@@ -2,7 +2,7 @@ import firebase from "firebase";
 import "firebase/auth";
 
 export const connection = {
-  apiKey: process.env.API_KEY
+  apiKey: process.env.FIREBASE_API_KEY
 };
 
 export const ui = {
