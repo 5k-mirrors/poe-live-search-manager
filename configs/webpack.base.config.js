8e9dc68398e0ec637b2e7d2407b46d3a1ba0f44a
Gömöri Zsolt <gomori.zsolt1995@gmail.com>
2019-07-16 12:55:30 +0200

    Reformat CI environment variable handling

--- configs/webpack.base.config.js
+++ configs/webpack.base.config.js
@@ -12,7 +12,12 @@ module.exports = {
       },
     ],
   },
-  plugins: [new DotEnv()],
+  plugins: [
+    new DotEnv({
+      safe: true,
+      systemvars: true,
+    }),
+  ],
   resolve: {
     // Reason for adding .json
     // => https://github.com/MarshallOfSound/electron-devtools-installer/pull/60#issuecomment-320229210
