840852c80715e059638d437e1fac9ff3bb0bcace
Csaba Apagyi <csaba.apagyi@gmail.com>
2019-07-20 17:38:45 +0200

    Change regex to match conventions

--- app/resources/RegExes/RegExes.js
+++ app/resources/RegExes/RegExes.js
@@ -1,6 +1,7 @@
-// https://rubular.com/r/Td2fd4EM4IJ671
+// => Prettier and ESlint force to remove the escape the characters from the RegEx.
+/* eslint-disable prettier/prettier, no-useless-escape */
+
 export const searchUrlLeagueAndIdMatcher = new RegExp(
-  // => Prettier and ESlint force to remove the escape the characters from the RegEx.
-  // eslint-disable-next-line prettier/prettier, no-useless-escape
-  "^(?:https?:\/\/)?(?:www.)?pathofexile.com\/trade\/search\/([a-zA-Z]*)\/([a-zA-Z0-9]*)(?:\/|\/live|\/live\/)?$"
+  // https://regex101.com/r/czwDtz/2
+  "^(?:https?:\/\/)?(?:www\.)?pathofexile\.com\/trade\/search\/([a-zA-Z]+?)\/([a-zA-Z0-9]+?)(?:\/|\/live|\/live\/)?$"
 );
