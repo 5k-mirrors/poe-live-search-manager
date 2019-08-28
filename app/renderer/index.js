2d151351717c8aeae93fc93ec0e03866c6b21a0e
gomorizsolt <gomori.zsolt1995@gmail.com>
2019-08-28 09:30:22 +0200

    Use HashRouter

--- app/renderer/index.js
+++ app/renderer/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import * as ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import App from "./containers/App/App";
 import setupIPCEvents from "./SetupIPCEvents/SetupIPCEvents";
 import * as firebaseUtils from "./utils/FirebaseUtils/FirebaseUtils";
