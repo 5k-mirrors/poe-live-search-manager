5f6570ec6c2d72a0a1633849ba2ceb98eb10e8c9
gomorizsolt <gomori.zsolt1995@gmail.com>
2019-08-28 14:13:41 +0200

    Move getApp() inside Firebase

--- app/renderer/components/Firebase/Firebase.js
+++ app/renderer/components/Firebase/Firebase.js
@@ -6,36 +6,36 @@ import { globalStore } from "../../../GlobalStore/GlobalStore";
 import { storeKeys } from "../../../resources/StoreKeys/StoreKeys";
 import { ipcEvents } from "../../../resources/IPCEvents/IPCEvents";
 
-const getApp = () => {
-  // https://stackoverflow.com/a/41005100/9599137
-  if (firebase.apps.length) {
-    return firebase.apps[0];
-  }
-
-  return firebase.initializeApp({
-    apiKey: process.env.FIREBASE_API_KEY,
-  });
-};
-
 const Firebase = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(null);
 
+  function getApp() {
+    // https://stackoverflow.com/a/41005100/9599137
+    if (firebase.apps.length) {
+      return firebase.apps[0];
+    }
+
+    return firebase.initializeApp({
+      apiKey: process.env.FIREBASE_API_KEY,
+    });
+  }
+
   function startAuthObserver() {
-    return getApp()
-      .auth()
-      .onAuthStateChanged(user => {
-        setCurrentUser(user);
+    const firebaseApp = getApp();
 
-        const isLoggedIn = !!user;
+    return firebaseApp.auth().onAuthStateChanged(user => {
+      setCurrentUser(user);
 
-        globalStore.set(storeKeys.IS_LOGGED_IN, isLoggedIn);
+      const isLoggedIn = !!user;
 
-        if (isLoggedIn) {
-          ipcRenderer.send(ipcEvents.USER_LOGIN, user.uid);
-        } else {
-          ipcRenderer.send(ipcEvents.USER_LOGOUT);
-        }
-      });
+      globalStore.set(storeKeys.IS_LOGGED_IN, isLoggedIn);
+
+      if (isLoggedIn) {
+        ipcRenderer.send(ipcEvents.USER_LOGIN, user.uid);
+      } else {
+        ipcRenderer.send(ipcEvents.USER_LOGOUT);
+      }
+    });
   }
 
   useEffect(() => {
