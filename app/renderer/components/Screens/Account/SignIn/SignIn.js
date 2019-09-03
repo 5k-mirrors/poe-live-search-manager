0859863bc46b1fcd64be6eeb7b007aa1efc38201
gomorizsolt <gomori.zsolt1995@gmail.com>
2019-09-03 13:37:25 +0200

    Improve sign-in screen

--- app/renderer/components/Screens/Account/SignIn/SignIn.js
+++ app/renderer/components/Screens/Account/SignIn/SignIn.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
 import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 import * as firebaseConfigs from "../../../../resources/FirebaseConfigs/FirebaseConfigs";
 import * as firebaseUtils from "../../../../utils/FirebaseUtils/FirebaseUtils";
@@ -7,13 +9,19 @@ const signIn = () => {
   const firebaseContext = firebaseUtils.useFirebaseContext();
 
   return (
-    <div>
-      <p>Sign in</p>
+    <Box textAlign="center">
+      <Typography paragraph variant="h5">
+        {/* eslint-disable-next-line react/jsx-one-expression-per-line */}
+        Register or Sign in to <b>poe-sniper</b>
+      </Typography>
+      <Box fontStyle="italic" component="p">
+        This is not your pathofexile.com account
+      </Box>
       <StyledFirebaseAuth
         uiConfig={firebaseConfigs.ui}
         firebaseAuth={firebaseContext.app.auth()}
       />
-    </div>
+    </Box>
   );
 };
 
