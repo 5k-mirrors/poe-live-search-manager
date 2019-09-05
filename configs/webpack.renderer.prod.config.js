f548989019b1c86262bde48b635ecf34c283976e
gomorizsolt <gomori.zsolt1995@gmail.com>
2019-09-05 10:55:44 +0200

    Fix typo

--- configs/webpack.renderer.prod.config.js
+++ configs/webpack.renderer.prod.config.js
@@ -1,8 +1,8 @@
 const path = require("path");
 const merge = require("webpack-merge");
-const webpackProdConfiguration = require("./webpack.base.prod.config");
+const webpackProdConfigurations = require("./webpack.base.prod.config");
 
-module.exports = merge(webpackProdConfiguration, {
+module.exports = merge(webpackProdConfigurations, {
   // => @babel/polyfill https://stackoverflow.com/a/33527883/9599137
   entry: ["@babel/polyfill", path.resolve("app", "renderer", "index.js")],
   output: {
