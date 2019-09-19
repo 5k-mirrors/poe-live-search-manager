31df71a7284a7022635b156decb1c70094f175a2
gomorizsolt <gomori.zsolt1995@gmail.com>
2019-09-19 14:11:41 +0200

    Remove NOTIFICATIONS_INTERVAL from global store

--- app/resources/StoreKeys/StoreKeys.js
+++ app/resources/StoreKeys/StoreKeys.js
@@ -3,7 +3,6 @@ export const storeKeys = {
   IS_LOGGED_IN: "isLoggedIn",
   RESULTS: "results",
   WS_CONNECTIONS: "wsConnections",
-  NOTIFICATIONS_INTERVAL: "notificationsInterval",
   COPY_WHISPER: "copyWhisper",
   RESULTS_LIMIT: "resultsLimit",
 };
