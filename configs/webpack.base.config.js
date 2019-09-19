fbfdebd1007be5991789d6b2a2b32d78fadc2d22
gomorizsolt <gomori.zsolt1995@gmail.com>
2019-09-19 12:51:14 +0200

    Use webpack.DefinePlugin to expose global constants

--- configs/webpack.base.config.js
+++ configs/webpack.base.config.js
@@ -1,8 +1,12 @@
+const webpack = require("webpack");
 const JavaScriptObfuscator = require("webpack-obfuscator");
 
-require("dotenv").config();
-
 const isProduction = process.env.NODE_ENV === "production";
+
+if (!isProduction) {
+  // eslint-disable-next-line global-require
+  require("dotenv").config();
+}
 
 module.exports = {
   mode: isProduction ? "production" : "development",
@@ -18,7 +22,19 @@ module.exports = {
       },
     ],
   },
-  plugins: isProduction ? [new JavaScriptObfuscator()] : [],
+  plugins: isProduction
+    ? [
+        new JavaScriptObfuscator(),
+        new webpack.DefinePlugin({
+          "process.env.FIREBASE_API_KEY": JSON.stringify(
+            process.env.FIREBASE_API_KEY
+          ),
+          "process.env.FIREBASE_API_URL": JSON.stringify(
+            process.env.FIREBASE_API_URL
+          ),
+        }),
+      ]
+    : [],
   resolve: {
     // Reason for adding .json
     // => https://github.com/MarshallOfSound/electron-devtools-installer/pull/60#issuecomment-320229210
