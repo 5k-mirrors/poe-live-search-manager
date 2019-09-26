88b1ba18e1605975eec0ab2cea5ec2364f8c5883
gomorizsolt <gomori.zsolt1995@gmail.com>
2019-09-26 12:00:19 +0200

    Accept encoded spaces in league

--- app/resources/RegExes/RegExes.js
+++ app/resources/RegExes/RegExes.js
@@ -3,7 +3,7 @@
 
 export const searchUrlLeagueAndIdMatcher = new RegExp(
   // https://regex101.com/r/czwDtz/2
-  "^(?:https?:\/\/)?(?:www\.)?pathofexile\.com\/trade\/search\/([a-zA-Z]+?)\/([a-zA-Z0-9]+?)(?:\/|\/live|\/live\/)?$"
+  "^(?:https?:\/\/)?(?:www\.)?pathofexile\.com\/trade\/search\/([a-zA-Z%]+?)\/([a-zA-Z0-9]+?)(?:\/|\/live|\/live\/)?$"
 );
 
 export const semanticVersionNumberMatcher = new RegExp("^v[0-9]\.[0-9]\.[0-9]$");
