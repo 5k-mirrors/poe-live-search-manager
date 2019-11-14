7734fa0a501ea8a44191f84d563b95cd06a9db8f
gomorizsolt <gomori.zsolt1995@gmail.com>
2019-11-14 15:03:19 +0100

    Display News in nav, make it the default screen

---
+++ app/renderer/components/Screens/News/News.js
@@ -0,0 +1,3 @@
+import React from "react";
+
+export default () => <h1>News</h1>;
--- app/renderer/resources/Routes/Routes.js
+++ app/renderer/resources/Routes/Routes.js
@@ -1,4 +1,5 @@
 export const routes = [
+  { displayName: "News", path: "/news" },
   { displayName: "Results", path: "/results" },
   { displayName: "Searches", path: "/searches" },
   { displayName: "Account", path: "/account" },
