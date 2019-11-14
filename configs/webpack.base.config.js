5bf99303798c052f04218b6e2541f2eed2749502
Csaba Apagyi <csaba.apagyi@gmail.com>
2019-11-14 01:52:34 +0100

    Simplify getting revision

--- configs/webpack.base.config.js
+++ configs/webpack.base.config.js
@@ -4,6 +4,11 @@ const DotEnv = require("dotenv-webpack");
 
 const isProduction = process.env.NODE_ENV === "production";
 
+const revision = require("child_process")
+  .execSync("git describe --always --tags")
+  .toString()
+  .trim();
+
 module.exports = {
   mode: isProduction ? "production" : "development",
   devtool: isProduction ? "" : "eval-source-map",
@@ -18,20 +23,25 @@ module.exports = {
       },
     ],
   },
-  plugins: isProduction
-    ? [
-        new JavaScriptObfuscator(),
-        new webpack.EnvironmentPlugin([
-          "FIREBASE_API_KEY",
-          "FIREBASE_API_URL",
-          "REVISION",
-        ]),
-      ]
-    : [
-        new DotEnv({
-          safe: true,
-        }),
-      ],
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env.REVISION": JSON.stringify(revision),
+    }),
+  ].concat(
+    isProduction
+      ? [
+          new JavaScriptObfuscator(),
+          new webpack.EnvironmentPlugin([
+            "FIREBASE_API_KEY",
+            "FIREBASE_API_URL",
+          ]),
+        ]
+      : [
+          new DotEnv({
+            safe: true,
+          }),
+        ]
+  ),
   resolve: {
     // Reason for adding .json
     // => https://github.com/MarshallOfSound/electron-devtools-installer/pull/60#issuecomment-320229210
