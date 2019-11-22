e85128f77b73a8e515c039d41c3931244190a441
gomorizsolt <gomori.zsolt1995@gmail.com>
2019-11-22 10:43:36 +0100

    Turn off 'no-underscore-dangle' rule after 'this' to follow class property name conventions

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,7 @@ module.exports = {
         "react/prop-types": 0,
         "linebreak-style": ["error", (process.platform === "win32" ? "windows" : "unix")], // https://stackoverflow.com/q/39114446/2771889
         "import/prefer-default-export": 0, // https://stackoverflow.com/q/54245654/2771889
-        "import/no-extraneous-dependencies": ["error", {"devDependencies": true}]
+        "import/no-extraneous-dependencies": ["error", {"devDependencies": true}],
+        "no-underscore-dangle": ["error", { "allowAfterThis": true }]
       },
 };
