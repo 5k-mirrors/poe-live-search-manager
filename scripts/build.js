86e51a0a5cc1bf5821208a61ed39988208e46d1b
Csaba Apagyi <csaba.apagyi@gmail.com>
2019-11-26 16:43:48 +0100

    Update build.js comments

--- scripts/build.js
+++ scripts/build.js
@@ -1,4 +1,4 @@
-// This file is used to set the custom `REVISION` env var, because sccripting in Windows is fucking awful
+// This file is used to set the custom env vars, because sccripting in Windows is fucking awful
 // https://stackoverflow.com/a/55284054/2771889
 
 const { execSync } = require("child_process");
@@ -15,7 +15,6 @@ const semverRegExp = new RegExp(/^v[0-9]\.[0-9]\.[0-9]$/);
 if (semverRegExp.test(process.argv[2])) {
   // We must adapt to electron-builder's static behavior when publishing releases because it heavily depends on env vars.
   // Setting the `REVISION` to `CI_BUILD_TAG` is mandatory to let electron-builder recognize it's indeed a tagged commit.
-  // https://github.com/electron-userland/electron-builder/blob/e9c70d50bb3b824e6ffe99965ec6acede55d2844/packages/electron-publish/src/publisher.ts#L98-L101
   // https://github.com/electron-userland/electron-builder/issues/4469
   env.CI_BUILD_TAG = env.REVISION;
 }
