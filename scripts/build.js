f6a2c460b07625cd2d9cd84d3503af3f3eee6932
gomorizsolt <gomori.zsolt1995@gmail.com>
2019-11-26 15:38:39 +0100

    Reference submitted issue to electron-builder

--- scripts/build.js
+++ scripts/build.js
@@ -15,6 +15,7 @@ if (semverRegExp.test(env.REVISION)) {
   // We must adapt to electron-builder's static behavior when publishing releases because it heavily depends on env vars.
   // Setting the `REVISION` to `CI_BUILD_TAG` is mandatory to let electron-builder recognize it's indeed a tagged commit.
   // https://github.com/electron-userland/electron-builder/blob/e9c70d50bb3b824e6ffe99965ec6acede55d2844/packages/electron-publish/src/publisher.ts#L98-L101
+  // https://github.com/electron-userland/electron-builder/issues/4469
   env.CI_BUILD_TAG = env.REVISION;
 }
 
