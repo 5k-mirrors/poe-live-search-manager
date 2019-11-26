61d8530f9f2bb49ef1300ec54d789d72fc6ff7a1
gomorizsolt <gomori.zsolt1995@gmail.com>
2019-11-26 17:33:53 +0100

    Add isTaggedCommit(), parse version

--- scripts/build.js
+++ scripts/build.js
@@ -1,4 +1,4 @@
-// This file is used to set the custom env vars, because sccripting in Windows is fucking awful
+// This file is used to set the custom `REVISION` env var, because sccripting in Windows is fucking awful
 // https://stackoverflow.com/a/55284054/2771889
 
 const { execSync } = require("child_process");
@@ -11,8 +11,14 @@ env.REVISION = execSync("git describe --always --tags --dirty")
 
 const semverRegExp = new RegExp(/^v[0-9]\.[0-9]\.[0-9]$/);
 
-// process.argv[2] represents `github.ref` coming from the CI.
-if (semverRegExp.test(process.argv[2])) {
+function isTaggedCommit() {
+  // process.argv[2] represents `github.ref` coming from the CI, e.g. refs/tags/v1.8.0.
+  const value = process.argv[2].split("/")[2];
+
+  return semverRegExp.test(value);
+}
+
+if (isTaggedCommit()) {
   // We must adapt to electron-builder's static behavior when publishing releases because it heavily depends on env vars.
   // Setting the `REVISION` to `CI_BUILD_TAG` is mandatory to let electron-builder recognize it's indeed a tagged commit.
   // https://github.com/electron-userland/electron-builder/issues/4469
