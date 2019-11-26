2db9504f597f3f56f737ec0ce4992aee5be2ee90
gomorizsolt <gomori.zsolt1995@gmail.com>
2019-11-26 16:43:17 +0100

    Pass 'github.ref' into build script to avoid duplicate releases

--- scripts/build.js
+++ scripts/build.js
@@ -11,7 +11,8 @@ env.REVISION = execSync("git describe --always --tags --dirty")
 
 const semverRegExp = new RegExp(/^v[0-9]\.[0-9]\.[0-9]$/);
 
-if (semverRegExp.test(env.REVISION)) {
+// process.argv[2] represents `github.ref` coming from the CI.
+if (semverRegExp.test(process.argv[2])) {
   // We must adapt to electron-builder's static behavior when publishing releases because it heavily depends on env vars.
   // Setting the `REVISION` to `CI_BUILD_TAG` is mandatory to let electron-builder recognize it's indeed a tagged commit.
   // https://github.com/electron-userland/electron-builder/blob/e9c70d50bb3b824e6ffe99965ec6acede55d2844/packages/electron-publish/src/publisher.ts#L98-L101
