1bab6071fea6ddb07244e1a5fec3e9e42629107e
gomorizsolt <gomori.zsolt1995@gmail.com>
2019-11-26 15:58:25 +0100

    Remove log step, set CI_BUILD_TAG env var when on tag

--- scripts/build.js
+++ scripts/build.js
@@ -9,4 +9,14 @@ env.REVISION = execSync("git describe --always --tags --dirty")
   .toString()
   .trim();
 
+const semverRegExp = new RegExp(/^v[0-9]\.[0-9]\.[0-9]$/);
+
+if (semverRegExp.test(env.REVISION)) {
+  // We must adapt to electron-builder's static behavior when publishing releases because it heavily depends on env vars.
+  // Setting the `REVISION` to `CI_BUILD_TAG` is mandatory to let electron-builder recognize it's indeed a tagged commit.
+  // https://github.com/electron-userland/electron-builder/blob/e9c70d50bb3b824e6ffe99965ec6acede55d2844/packages/electron-publish/src/publisher.ts#L98-L101
+  // https://github.com/electron-userland/electron-builder/issues/4469
+  env.CI_BUILD_TAG = env.REVISION;
+}
+
 execSync("yarn package:win", { env, stdio: "inherit" });
