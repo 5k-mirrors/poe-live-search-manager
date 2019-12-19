a71df0969f6f1fb65f72b71d5676ff69339c0025
gomorizsolt <gomori.zsolt1995@gmail.com>
2019-12-19 16:57:42 +0100

    Use AuthContext in loggedIn

--- app/renderer/components/Screens/Account/LoggedIn/LoggedIn.js
+++ app/renderer/components/Screens/Account/LoggedIn/LoggedIn.js
@@ -5,26 +5,35 @@ import SessionIdEditor from "./SessionIdEditor/SessionIdEditor";
 import SubscriptionDetails from "./SubscriptionDetails/SubscriptionDetails";
 import Button from "../../../UI/SimpleHtmlElements/Button/Button";
 import { getApp as getFirebaseApp } from "../../../../utils/Firebase/Firebase";
+import { useAuthContext } from "../../../../contexts/Auth";
 
-const firebaseAuth = getFirebaseApp().auth();
+const loggedIn = () => {
+  const auth = useAuthContext();
 
-const loggedIn = () => (
-  <div>
-    <Box
-      display="flex"
-      alignItems="center"
-      mb={3}
-      justifyContent="space-between"
-    >
-      <Typography variant="h6" gutterBottom>
-        {`Logged in as ${firebaseAuth.currentUser.displayName ||
-          firebaseAuth.currentUser.email}`}
-      </Typography>
-      <Button clickEvent={() => firebaseAuth.signOut()} text="Sign out" />
-    </Box>
-    <SessionIdEditor />
-    <SubscriptionDetails />
-  </div>
-);
+  return (
+    <div>
+      <Box
+        display="flex"
+        alignItems="center"
+        mb={3}
+        justifyContent="space-between"
+      >
+        <Typography variant="h6" gutterBottom>
+          {`Logged in as ${auth.data.displayName || auth.data.email}`}
+        </Typography>
+        <Button
+          clickEvent={() =>
+            getFirebaseApp()
+              .auth()
+              .signOut()
+          }
+          text="Sign out"
+        />
+      </Box>
+      <SessionIdEditor />
+      <SubscriptionDetails />
+    </div>
+  );
+};
 
 export default loggedIn;
