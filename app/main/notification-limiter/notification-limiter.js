752ef092fcdedc0fbbc3db87d6c57db34c8de989
gomorizsolt <gomori.zsolt1995@gmail.com>
2020-01-22 16:21:14 +0100

    Add clarification comment on why Bottleneck.Group() is used

--- app/main/notification-limiter/notification-limiter.js
+++ app/main/notification-limiter/notification-limiter.js
@@ -1,5 +1,7 @@
 import Bottleneck from "bottleneck";
 
+// Even though Bottleneck is capable of shutting down a limiter and dropping scheduled jobs, it can't be restarted to receive jobs once it's been stopped.
+// Group is used instead. This way the limiters are isolated, can be created and dropped one-by-one if the user decides to turn off notifications.
 class NotificationLimiter {
   constructor() {
     // The limit is set to 7500ms to align with Windows notifications(5 seconds by default, but the notification's effect lasts for ~2500 ms).
