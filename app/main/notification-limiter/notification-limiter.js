49211fb05afff1346e8c2157c0cccc06b68579d3
gomorizsolt <gomori.zsolt1995@gmail.com>
2020-01-23 11:03:13 +0100

    Fix typo

--- app/main/notification-limiter/notification-limiter.js
+++ app/main/notification-limiter/notification-limiter.js
@@ -1,6 +1,6 @@
 import Bottleneck from "bottleneck";
 
-// Notifications are limited to adapt to Windows's default settings. Users can enable and disable notifications which means the limiter needs to be stopped and restarted several times
+// Notifications are limited to adapt to Windows's default settings. Users can enable and disable notifications which means the limiter needs to be stopped and restarted several times.
 // Even though Bottleneck is capable of shutting down and dropping scheduled jobs altogether, it can't restart a particular limiter to receive jobs once it's stopped.
 // The group feature is used instead even though there's always a single limiter attached to the group. This single limiter can be removed and re-added in response to user interactions.
 export default class NotificationsLimiter {
@@ -17,6 +17,7 @@ export default class NotificationsLimiter {
   }
 
   // It stops the limiter attached to the group which means all queued jobs are dropped regardless of their state. Once it resolves, the limiter under the specified key is removed.
+  // https://github.com/SGrondin/bottleneck#stop
   static drop() {
     return this.get()
       .stop()
