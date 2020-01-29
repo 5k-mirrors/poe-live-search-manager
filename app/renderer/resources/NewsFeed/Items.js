5df36cf3f6f0837a7db451fa47c3454e7bf5e5e7
Csaba Apagyi <csaba.apagyi@gmail.com>
2020-01-30 00:45:54 +0100

    Bump version

--- app/renderer/resources/NewsFeed/Items.js
+++ app/renderer/resources/NewsFeed/Items.js
@@ -7,7 +7,7 @@ export default [
     description: `The live search functionality requires a Session ID. In general, it's not safe to give out your Session ID as it can be used to impersonate your account. In our case, you can provide any Session ID (e.g. one from a new, empty account). The app only stores your Session ID locally and only uses it to access the live search function.
 
 Stay safe exiles!`,
-    date: "2020-01-29T23:00:00Z",
+    date: "2020-01-29T23:30:00Z",
   },
   {
     type: types.RELEASE_NOTE,
