6af22b57a07c42d5050eee57798fdbb2f95808c8
Csaba Apagyi <csaba.apagyi@gmail.com>
2020-01-30 00:04:19 +0100

    Change news type to be generic

--- app/renderer/components/Screens/News/News.js
+++ app/renderer/components/Screens/News/News.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Paper from "@material-ui/core/Paper";
-import { ReleaseNote } from "./Types";
+import { Announcement } from "./Types";
 import { useNewsStyles } from "./News.style";
 import { newsFeedItems, newsFeedItemTypes } from "../../../resources/NewsFeed";
 import { devErrorLog } from "../../../../utils/JavaScriptUtils/JavaScriptUtils";
@@ -13,7 +13,19 @@ export default () => {
       {newsFeedItems.map(itemDetails => {
         switch (itemDetails.type) {
           case newsFeedItemTypes.RELEASE_NOTE:
-            return <ReleaseNote key={itemDetails.title} {...itemDetails} />;
+            return (
+              <Announcement
+                key={itemDetails.title + itemDetails.date}
+                {...itemDetails}
+              />
+            );
+          case newsFeedItemTypes.ANNOUNCEMENT:
+            return (
+              <Announcement
+                key={itemDetails.title + itemDetails.date}
+                {...itemDetails}
+              />
+            );
           default:
             devErrorLog(
               `${itemDetails.type} type is not supported on news feed.`
