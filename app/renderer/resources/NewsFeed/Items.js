1258cf7f887a652ff9d3210f0abc7d48bc85e35d
Csaba Apagyi <csaba.apagyi@gmail.com>
2020-01-30 01:50:01 +0100

    Add announcement about connection limits

--- app/renderer/resources/NewsFeed/Items.js
+++ app/renderer/resources/NewsFeed/Items.js
@@ -3,11 +3,17 @@ import types from "./Types";
 export default [
   {
     type: types.ANNOUNCEMENT,
-    title: "IMPORTANT ANNOUNCEMENT",
-    description: `The live search functionality requires a Session ID. In general, it's not safe to give out your Session ID as it can be used to impersonate your account. In our case, you can provide any Session ID (e.g. one from a new, empty account). The app only stores your Session ID locally and only uses it to access the live search function.
+    title: "IMPORTANT ANNOUNCEMENT ABOUT SESSION ID",
+    description: `The live search functionality requires a Session ID. In general, it's not safe to give out your Session ID because it can be used to impersonate your account. In our case, you can provide any Session ID (e.g. one from a new, empty account). The app only stores your Session ID locally and only uses it for live searches.
 
 Stay safe exiles!`,
     date: "2020-01-29T23:30:00Z",
+  },
+  {
+    type: types.ANNOUNCEMENT,
+    title: "IMPORTANT ANNOUNCEMENT ABOUT CONNECTION LIMITS",
+    description: `The limit of active live searches is now 20 per account. This is the limitation of pathofexile.com/trade, also applies in the browser. Don't add more.`,
+    date: "2020-01-29T23:10:00Z",
   },
   {
     type: types.RELEASE_NOTE,
