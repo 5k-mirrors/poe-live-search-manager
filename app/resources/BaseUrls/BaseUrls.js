9df44ae1d9b13f5ee94a0b619676a5855456f7a8
gomorizsolt <gomori.zsolt1995@gmail.com>
2020-02-04 13:49:13 +0100

    Remove unused firebaseUserAPI from base URLs

--- app/resources/BaseUrls/BaseUrls.js
+++ app/resources/BaseUrls/BaseUrls.js
@@ -1,6 +1,3 @@
-export const firebaseUserAPI =
-  "https://us-central1-poe-sniper-gateway.cloudfunctions.net/api";
-
 export const poeFetchAPI = "https://www.pathofexile.com/api/trade/fetch/";
 
 export const poeWsUri = "wss://www.pathofexile.com/api/trade/live";
