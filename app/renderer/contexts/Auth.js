8a8715dd01675b26f15bae4197daada640fc6def
gomorizsolt <gomori.zsolt1995@gmail.com>
2020-02-14 10:09:26 +0100

    Clean up the interval upon sign-outs

--- app/renderer/contexts/Auth.js
+++ app/renderer/contexts/Auth.js
@@ -105,6 +105,8 @@ export const AuthProvider = ({ children }) => {
               isLoggedIn: false,
             },
           });
+
+          clearInterval(intervalId.current);
         }
       });
     };
