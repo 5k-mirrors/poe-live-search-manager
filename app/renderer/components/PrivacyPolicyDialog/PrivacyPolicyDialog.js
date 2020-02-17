246cf28d85ef63c88d800398bd0d1569b1876692
gomorizsolt <gomori.zsolt1995@gmail.com>
2020-02-17 14:11:26 +0100

    Add checkbox

--- app/renderer/components/PrivacyPolicyDialog/PrivacyPolicyDialog.js
+++ app/renderer/components/PrivacyPolicyDialog/PrivacyPolicyDialog.js
@@ -5,10 +5,13 @@ import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
 import { useFirebaseAuthObserver } from "../../utils/CustomHooks/CustomHooks";
 
 export default () => {
   const [showDialog, setShowDialog] = useState(false);
+  const [checked, setChecked] = useState(false);
   const { authenticated } = useFirebaseAuthObserver();
 
   useEffect(() => {
@@ -21,9 +24,26 @@ export default () => {
     <Dialog open={showDialog}>
       <DialogTitle>Privacy Policy</DialogTitle>
       <DialogContent>
-        <DialogContentText>Text</DialogContentText>
+        <DialogContentText>
+          <FormControlLabel
+            control={
+              // eslint-disable-next-line react/jsx-wrap-multilines
+              <Checkbox
+                checked={checked}
+                onChange={() => setChecked(prevChecked => !prevChecked)}
+                color="primary"
+              />
+            }
+            label="I have read and I accept the Privacy Policy"
+          />
+        </DialogContentText>
         <DialogActions>
-          <Button onClick={() => setShowDialog(false)} color="primary">
+          <Button
+            onClick={() => setShowDialog(false)}
+            color="primary"
+            variant="contained"
+            disabled={!checked}
+          >
             Agree
           </Button>
         </DialogActions>
