5c67cbecf26e24ee2e2cf88d13887ad21fc12ee8
Csaba Apagyi <csaba.apagyi@gmail.com>
2020-04-22 13:45:39 +0200

    Add support announcement

--- app/renderer/resources/NewsFeed/Items.js
+++ app/renderer/resources/NewsFeed/Items.js
@@ -2,6 +2,17 @@ import types from "./Types";
 
 export default [
   {
+    type: types.ANNOUNCEMENT,
+    title: "Support PoE Live Search Manager",
+    link: "https://github.com/sponsors/thisismydesign",
+    description: `Keep the tool alive and growing! In the past year, new features were created by paid developers. You can now support us via the [Sponsor button](https://github.com/sponsors/thisismydesign) on [GitHub](https://github.com/5k-mirrors/poe-live-search-manager).
+
+All contributions go toward improving the tool. If you have a specific feature request, or any questions, feel free to reach out at [git.thisismydesign@gmail.com](mailto:git.thisismydesign@gmail.com).
+
+This is similar to Patreon but GitHub doesn't take a cut, rather initially they will even [match your contribution](https://help.github.com/en/github/supporting-the-open-source-community-with-github-sponsors/about-github-sponsors#about-the-github-sponsors-matching-fund).`,
+    date: "2020-04-22T13:30:00Z",
+  },
+  {
     type: types.RELEASE_NOTE,
     title: "v1.11.4",
     link:
