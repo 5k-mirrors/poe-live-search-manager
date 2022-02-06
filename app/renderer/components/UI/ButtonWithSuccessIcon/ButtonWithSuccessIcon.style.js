d00383084732a7a41a36f4f92253c05a7f989907
Csaba Apagyi <csaba.apagyi@gmail.com>
2022-02-06 20:49:00 +0100

    Restore makeStyles behavior

--- app/renderer/components/UI/ButtonWithSuccessIcon/ButtonWithSuccessIcon.style.js
+++ app/renderer/components/UI/ButtonWithSuccessIcon/ButtonWithSuccessIcon.style.js
@@ -1,12 +1,10 @@
 import makeStyles from "@mui/styles/makeStyles";
 
-export const useButtonWithSuccessIconStyles = () => {
-  return makeStyles(theme => ({
-    button: {
-      fontWeight: "bold",
-    },
-    extendedIcon: {
-      marginLeft: theme.spacing(1),
-    },
-  }));
-};
+export const useButtonWithSuccessIconStyles = makeStyles(theme => ({
+  button: {
+    fontWeight: "bold",
+  },
+  extendedIcon: {
+    marginLeft: theme.spacing(1),
+  },
+}));
