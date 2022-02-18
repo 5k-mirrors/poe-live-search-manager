3cd55dc62bb136d216b5a9c2401a78baeb592776
Csaba Apagyi <csaba.apagyi@gmail.com>
2022-02-18 14:23:09 +0100

    Update announcement

--- app/renderer/resources/Announcements/Announcements.js
+++ app/renderer/resources/Announcements/Announcements.js
@@ -1,12 +1,9 @@
 export default [
   {
-    title: "Support PoE Live Search Manager",
+    title: "Support the development",
     link: "https://github.com/sponsors/thisismydesign",
-    description: `Keep the tool alive and growing! In the past year, new features were created by paid developers. You can now support us via the [Sponsor button](https://github.com/sponsors/thisismydesign) on [GitHub](https://github.com/5k-mirrors/poe-live-search-manager).
-
-All contributions go toward improving the tool. If you have a specific feature request, or any questions, feel free to reach out at [git.thisismydesign@gmail.com](mailto:git.thisismydesign@gmail.com).
-
-This is similar to Patreon but GitHub doesn't take a cut, rather initially they will even [match your contribution](https://help.github.com/en/github/supporting-the-open-source-community-with-github-sponsors/about-github-sponsors#about-the-github-sponsors-matching-fund).`,
-    date: "2020-04-22T22:30:00Z",
+    description:
+      "Keep the tool alive and growing. You can support us via the [Sponsor button](https://github.com/sponsors/thisismydesign) on [GitHub](https://github.com/5k-mirrors/poe-live-search-manager).",
+    date: "2022-02-17T22:30:00Z",
   },
 ];
