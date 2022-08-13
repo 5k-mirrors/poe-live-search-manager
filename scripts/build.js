6a7d5844ac1b088d4e52bc88adae6bb4ad3bfa84
Csaba Apagyi <csaba.apagyi@gmail.com>
2022-08-13 12:23:33 +0200

    Simplify build step

--- scripts/build.js
+++ scripts/build.js
@@ -1,4 +1,4 @@
-// This file is used to set the custom `REVISION` env var, because sccripting in Windows is fucking awful
+// This file is used for cross-platform env var setting
 // https://stackoverflow.com/a/55284054/2771889
 
 const { execSync } = require("child_process");
@@ -11,27 +11,5 @@ const env = Object.create(process.env);
 env.REVISION = execSync("git describe --always --tags --dirty")
   .toString()
   .trim();
-
-const semverRegExp = new RegExp(/^v[0-9]+\.[0-9]+\.[0-9]+$/);
-
-function isTaggedCommit() {
-  // process.argv[2] represents `github.ref` coming from the CI, e.g. refs/tags/v1.8.0.
-  if (!process.argv[2]) return false;
-
-  // eslint-disable-next-line no-console
-  console.log("Build args:", process.argv);
-
-  const gitRef = process.argv[2];
-  const gitTag = gitRef.split("/")[2];
-
-  return semverRegExp.test(gitTag);
-}
-
-if (isTaggedCommit()) {
-  // eslint-disable-next-line no-console
-  console.log("Tagged commit:", env.REVISION);
-  // https://github.com/electron-userland/electron-builder/issues/4469
-  env.CI_BUILD_TAG = env.REVISION;
-}
 
 execSync("yarn package:win", { env, stdio: "inherit" });
